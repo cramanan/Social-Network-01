a1fe15d915d3f321b14563f9096ba20708653961
cramanan <cramananjaonapro@gmail.com>
2024-09-03 10:25:42 +0200

    feat: added models structures

--- api/models/posts.go
+++ api/models/posts.go
@@ -1,1 +1,17 @@
 package models
+
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
+
+type Post struct {
+	Id         uuid.UUID
+	UserId     uuid.UUID
+	GroupId    uuid.UUID
+	Categories []byte // JSON
+	Content    string
+	ImagePath  []byte // JSON
+	Timestamp  time.Time
+}
--- api/models/users.go
+++ api/models/users.go
@@ -1,1 +1,21 @@
 package models
+
+import (
+	"time"
+
+	"github.com/gofrs/uuid"
+)
+
+type User struct {
+	Id          uuid.UUID
+	Nickname    string
+	Email       string
+	password    []byte
+	FirstName   string
+	LastName    string
+	DateOfBirth time.Time
+	ImagePath   *string
+	AboutMe     *string
+	Private     bool
+	Timestamp   time.Time
+}
