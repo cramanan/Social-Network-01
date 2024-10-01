490f07629a982989a573cec2a633ec13c2dc70cf
cramanan <cramananjaonapro@gmail.com>
2024-10-01 12:21:00 +0200

    feat: changed next structure

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
-const API_URL = process.env.API_URL;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async rewrites() {
-        return [
-            {
-                source: "/api/:path*",
-                destination: `${API_URL}/api/:path*`,
-            },
-        ];
-    },
-};
+const nextConfig = {};
 
 export default nextConfig;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {},
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
