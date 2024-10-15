54b314973b4ae95c5d796224658afa74d2ce181e
dleboyer <oubbahidoreen@gmail.com>
2024-10-15 18:16:25 +0200

    create post finish

--- src/components/icons/newPost.tsx
+++ src/components/icons/newPost.tsx
@@ -2,6 +2,9 @@
 
 import React, { useState } from "react";
 import { ProfileCircle } from "./ProfileCircle";
+import { SendPostIcon } from "./sendPostIcon";
+import { CloseIcon } from "./CloseIcon";
+import { ImageIcon } from "./ImageIcon";
 
 export const NewPost = () => {
   const [postText, setPostText] = useState("");
@@ -32,32 +35,40 @@ export const NewPost = () => {
         </button>
       </div>
       {isModalOpen && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 backdrop-blur-sm h-full  z-50">
-          <div className=" relative border border-white bg-gradient-to-tr from-[#9ac0fa] to-[#efc0f0d7] p-6 rounded-lg shadow-lg w-1/2">
-            <ProfileCircle className="absolute  top-4 left-9 " />
-            <h2 className="text-l text-white font-semibold mb-4 flex justify-center ">
-              Create your Post
-            </h2>
+        <div className="  fixed inset-0 flex items-center justify-center bg-black bg-opacity-70 backdrop-blur-sm  z-50">
+          <div className=" border border-white bg-gradient-to-tr from-[#9ac0fa] to-[#efc0f0d7] p-6 rounded-lg shadow-lg  w-1/2 ">
+            <div className="flex justify-between">
+              <h2 className="text-xl text-white font-semibold flex justify-center items-center gap-4 ">
+                <ProfileCircle />
+                New Post
+              </h2>
+              <button
+                className="w-10 h-10 flex justify-center items-center hover:bg-gradient-to-tr from-[#9ac0fa] to-[#efc0f0d7]"
+                onClick={toggleModal}
+              >
+                <CloseIcon />
+              </button>
+            </div>
 
             <textarea
-              className="w-full  rounded-xl border border-gray-300  text-white text-xl justify-start items-center gap-2.5 inline-flex mb-4 placeholder-white"
+              className=" shadow-lg w-full px-12 py-4 mt-7 rounded-xl  bg-white text-black text-xl justify-start items-center gap-2.5 inline-flex mb-4 placeholder-gray-500 resize-none"
               value={postText}
               onChange={handleTextChange}
               placeholder="What's on your mind?"
             />
-            <div className="flex justify-end">
-              <button
-                className="px-4 py-2 bg-gray-500 text-white rounded mr-2"
-                onClick={toggleModal}
-              >
-                Cancel
+
+            <div className="flex justify-between">
+              <button className="flex flex-row gap-2 shadow-xl bg-slate-300  hover:bg-gradient-to-tr from-[#9ac0fa] to-[#efc0f0d7] text-black rounded justify-center items-center px-2 py-1">
+                upload
+                <ImageIcon />
               </button>
               <button
-                className="px-4 py-2 bg-blue-500 text-white rounded"
+                className=" flex flex-row gap-2 shadow-xl bg-slate-300  hover:bg-gradient-to-tr from-[#9ac0fa] to-[#efc0f0d7] text-black rounded justify-center items-center px-2 py-1"
                 type="submit"
                 onClick={handleSubmit}
               >
-                Submit
+                Send Post
+                <SendPostIcon />
               </button>
             </div>
           </div>
