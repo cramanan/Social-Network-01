4a7b25991e12edbd09998df2a125194088939401
cramanan <cramananjaonapro@gmail.com>
2024-10-31 14:37:36 +0100

    chore: removed loading on context

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -61,13 +61,6 @@ export default function AuthProvider({ children }: { children: ReactNode }) {
         login();
     }, []);
 
-    if (loading)
-        children = (
-            <div className="h-full w-full flex items-center justify-center">
-                Loading...
-            </div>
-        );
-
     return (
         <authContext.Provider value={{ user, loading, signup, login, logout }}>
             {children}
