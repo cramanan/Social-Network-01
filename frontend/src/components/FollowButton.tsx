1020189854b280b8eb4e93c97fd63f966a7e9e89
ayung <yung.anthony26@gmail.com>
2024-11-27 10:21:14 +0100

    update: Added accept/decline Friend Rquest

--- frontend/src/components/FollowButton.tsx
+++ frontend/src/components/FollowButton.tsx
@@ -4,16 +4,14 @@ import React from "react";
 
 export default function FollowButton({
     id,
-    nickname,
 }: {
     id: string;
-    nickname: string;
 }) {
     const follow = () =>
         fetch(`/api/user/${id}/send-request`, { method: "POST" });
     return (
         <button className="bg-gray-300 w-fit rounded-xl p-2" onClick={follow}>
-            Follow {nickname}
+            Follow / Unfollow
         </button>
     );
 }
--- frontend/src/components/FriendInvite.tsx
+++ frontend/src/components/FriendInvite.tsx
@@ -1,15 +1,24 @@
+'use client'
+
 import { User } from "@/types/user"
 import Image from "next/image"
 import React from 'react'
 
-const FriendInvite = ({ nickname, image }: User) => {
+const FriendInvite = ({ id, nickname, image }: User) => {
+    const acceptRequest = () =>
+        fetch(`/api/user/${id}/accept-request`, { method: "POST" });
+
+    const declineRequest = () =>
+        fetch(`/api/user/${id}/decline-request`, { method: "POST" });
+    console.log(id);
+
     return (
         <>
             <div className='w-60 flex flex-row justify-between items-center'>
                 <Image src={image} alt="" width={40} height={40} ></Image>
                 <div className='w-32'>{nickname}</div>
-                <div>✓</div>
-                <div>X</div>
+                <button onClick={acceptRequest}>✓</button>
+                <button onClick={declineRequest}>X</button>
             </div>
         </>
     )
